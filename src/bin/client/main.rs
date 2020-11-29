d176c5b57776c2e31f67aad7ee81b76662188eea
aegroto <aegroto@protonmail.com>
2020-11-29 16:18:28 +0100

    simple udt implementation, very bad performances

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -4,13 +4,34 @@ use std::str::FromStr;
 use std::net::{SocketAddr, SocketAddrV4};
 use udt::*;
 
-// const PACKET_SIZE: usize = 512;
-const FRAME_SIZE: usize = 1920 * 1080 * 4;
+use beryllium::*;
+use pixels::{wgpu::Surface, Pixels, SurfaceTexture};
 
-fn main() {
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+
+// const PACKET_SIZE: usize = 512;
+const FRAME_SIZE: usize = (WIDTH as usize) * (HEIGHT as usize) * 4;
+const RECV_BUFFER_SIZE: i32 = (FRAME_SIZE * 4) as i32;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Init display
+    let sdl = SDL::init(InitFlags::default())?;
+    let window =
+        sdl.create_raw_window("Remotia client", WindowPosition::Centered, WIDTH, HEIGHT, 0)?;
+
+    let mut pixels = {
+        let surface = Surface::create(&window);
+        let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, surface);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    pixels.render()?;
+
+    // Init socket
     let localhost = std::net::Ipv4Addr::from_str("127.0.0.1").unwrap();
 
-    let sock = UdtSocket::new(SocketFamily::AFInet, SocketType::Stream).unwrap();
+    let sock = UdtSocket::new(SocketFamily::AFInet, SocketType::Datagram).unwrap();
     sock.bind(SocketAddr::V4(SocketAddrV4::new(localhost, 5001))).unwrap();
     let my_addr = sock.getsockname().unwrap();
     println!("Server bound to {:?}", my_addr);
@@ -19,13 +40,18 @@ fn main() {
     println!("Received new connection from peer {:?}", peer);
 
     // let mut packet_buffer: [u8; PACKET_SIZE] = [0; PACKET_SIZE];
-    let mut frame_buffer: [u8; FRAME_SIZE] = [0; FRAME_SIZE];
+    // let mut frame_buffer: [u8; FRAME_SIZE] = [0; FRAME_SIZE];
+
+    connection_socket.setsockopt(UdtOpts::UDP_RCVBUF, RECV_BUFFER_SIZE).unwrap_err();
+    connection_socket.setsockopt(UdtOpts::UDT_RCVBUF, RECV_BUFFER_SIZE).unwrap_err();
 
     loop {
-        println!("Waiting for next frame...");
+        println!("Waiting for next frame (expected length: {})...", FRAME_SIZE);
 
-        connection_socket.recv(&mut frame_buffer, FRAME_SIZE).unwrap();
+        // let read_bytes = connection_socket.recv(pixels.get_frame(), FRAME_SIZE).unwrap();
+        let read_bytes = connection_socket.recvmsg(pixels.get_frame()).unwrap();
+        pixels.render()?;
 
-        println!("Received a frame");
+        println!("Received a frame (read {} bytes)", read_bytes);
     }
 }
--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -6,7 +6,7 @@ use scrap::{Capturer, Display, Frame};
 use std::time::{Duration, Instant};
 use std::thread;
 
-use std::cmp::min;
+// use std::cmp::min;
 
 // use std::net::UdpSocket;
 
@@ -14,7 +14,13 @@ use std::str::FromStr;
 use std::net::{SocketAddr, SocketAddrV4};
 use udt::*;
 
-const PACKET_SIZE: usize = 512;
+// const PACKET_SIZE: usize = 512;
+
+const WIDTH: u32 = 1280;
+const HEIGHT: u32 = 720;
+
+const FRAME_SIZE: usize = (WIDTH as usize) * (HEIGHT as usize) * 4;
+const SEND_BUFFER_SIZE: i32 = (FRAME_SIZE * 4) as i32;
 
 fn main() -> std::io::Result<()> {
     let display = Display::primary().expect("Couldn't find primary display.");
@@ -23,29 +29,35 @@ fn main() -> std::io::Result<()> {
     const FPS: u32 = 60;
     let spin_time = Duration::new(1, 0) / FPS;
 
-    let fast_socket = UdtSocket::new(SocketFamily::AFInet, SocketType::Stream).unwrap();
+    let fast_socket = UdtSocket::new(SocketFamily::AFInet, SocketType::Datagram).unwrap();
     let client_ipv4_address = std::net::Ipv4Addr::from_str("127.0.0.1").unwrap();
 
     fast_socket.connect(SocketAddr::V4(SocketAddrV4::new(client_ipv4_address, 5001))).unwrap();
+    fast_socket.setsockopt(UdtOpts::UDP_SNDBUF, SEND_BUFFER_SIZE).unwrap_err();
+    fast_socket.setsockopt(UdtOpts::UDT_SNDBUF, SEND_BUFFER_SIZE).unwrap_err();
 
     loop {
         let loop_start_time = Instant::now();
 
         // Capture frame
-        let result = capture::capture_frame(&mut capturer);
+        // let result = capture::capture_frame(&mut capturer);
 
-        let frame_buffer: Frame = match result {
-            Ok(buffer) => buffer,
-            Err(_error) => {
-                thread::sleep(spin_time);
-                continue;
-            }
-        };
+        // let frame_buffer: Frame = match result {
+        //     Ok(buffer) => buffer,
+        //     Err(_error) => {
+        //         thread::sleep(spin_time);
+        //         continue;
+        //     }
+        // };
+        let frame_buffer = vec![128; FRAME_SIZE];
 
         let transfer_start_time = Instant::now();
 
-        fast_socket.send(&frame_buffer).unwrap();
+        // let sent_bytes = fast_socket.send(&frame_buffer).unwrap();
+        let sent_bytes = fast_socket.sendmsg(&frame_buffer).unwrap();
 
+        let pending_buffer_data = fast_socket.getsockopt(UdtOpts::UDT_SNDDATA).unwrap();
+        println!("Sent {}/{} bytes. Pending {} bytes", sent_bytes, &frame_buffer.len(), pending_buffer_data);
         println!("Transfer time: {}", transfer_start_time.elapsed().as_millis());
 
         println!("Total time: {}", loop_start_time.elapsed().as_millis());

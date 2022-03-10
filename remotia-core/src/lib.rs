921bde48e3960fd9a0e355a2bf9fcae4891a6fbe
aegroto <aegroto@protonmail.com>
2022-03-10 16:20:31 +0100

    remotia crate converted to be a facade, actual code moved to remotia-core

---
+++ remotia-core/src/lib.rs
@@ -0,0 +1,17 @@
+#![allow(unused_imports)]
+#![feature(portable_simd)]
+
+//! remotia is a pure Rust framework to design remote rendering and general video streaming pipelines as code
+//! in a modular and intuitive way that makes it simple to alter and reuse components.
+//! The project is in early stage and documentation is in progress, but it is already usable for experimentations
+//! and to develop prototypes of cloud gaming software without relying on specific APIs.
+
+pub mod common;
+
+pub mod pipeline;
+
+pub mod traits;
+pub mod types;
+pub mod error;
+
+pub mod processors;

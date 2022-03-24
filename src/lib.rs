9d82346d3bd5084cbfb5c33c619df4f114dc9266
aegroto <aegroto@protonmail.com>
2022-03-24 09:58:50 +0100

    added public use declarations for every crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
 pub use remotia_core::*;
+pub use remotia_buffer_utils::*;
+pub use remotia_core_capturers::*;
+pub use remotia_core_codecs::*;
+pub use remotia_core_loggers::*;
+pub use remotia_core_renderers::*;
+pub use remotia_profilation_utils::*;

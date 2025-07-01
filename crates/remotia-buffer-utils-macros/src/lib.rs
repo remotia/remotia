c451d47ce103e94e8126f6e05cafe1d2d908ad75
aegroto <aegroto@protonmail.com>
2025-07-01 23:32:33 +0200

    feat: buffers_map supporting generic key name

--- crates/remotia-buffer-utils-macros/src/lib.rs
+++ crates/remotia-buffer-utils-macros/src/lib.rs
@@ -1,8 +1,10 @@
 extern crate proc_macro;
 
+use core::panic;
+
 use proc_macro::TokenStream;
-use proc_macro2::Span;
-use quote::quote;
+use proc_macro2::{Span, TokenTree};
+use quote::{quote, ToTokens};
 use syn::{parse_macro_input, Ident, ItemStruct};
 
 #[proc_macro_attribute]
@@ -16,16 +18,43 @@ pub fn buffers_map(attr: TokenStream, input: TokenStream) -> TokenStream {
     // Get the name of the struct
     let name = input.ident.clone();
 
+    // Find the field in the struct by its name
+    let field = input
+        .fields
+        .iter()
+        .find(|f| f.ident.as_ref() == Some(&field_name))
+        .unwrap();
+
+    // Extract the type of the field
+    let field_type_tokens = &field
+        .ty
+        .to_token_stream()
+        .into_iter()
+        .collect::<Vec<TokenTree>>();
+
+    let ty = field_type_tokens
+        .get(0)
+        .expect("Unable to read field first token");
+
+    if ty.to_string() != "BuffersMap" {
+        panic!("The field should be of type BuffersMap");
+    }
+
+    let key_type_name = Ident::new(&field_type_tokens
+        .get(2)
+        .expect("Unable to read key type")
+        .to_string(), Span::call_site());
+
     // Generate the implementation of PullableFrameProperties
     let expanded = quote! {
         #input
 
-        impl remotia::traits::PullableFrameProperties<Buffer, BytesMut> for #name {
-            fn push(&mut self, key: Buffer, value: BytesMut) {
+        impl remotia::traits::PullableFrameProperties<#key_type_name, BytesMut> for #name {
+            fn push(&mut self, key: #key_type_name, value: BytesMut) {
                 self.#field_name.insert(key, value);
             }
 
-            fn pull(&mut self, key: &Buffer) -> Option<BytesMut> {
+            fn pull(&mut self, key: &#key_type_name) -> Option<BytesMut> {
                 self.#field_name.remove(key)
             }
         }

30eedf807bbaa6f915759d2416769be735c3ef06
aegroto <aegroto@protonmail.com>
2025-09-04 16:03:50 +0200

    feat: updated winit renderer

--- crates/remotia-core-renderers/src/winit.rs
+++ crates/remotia-core-renderers/src/winit.rs
@@ -1,41 +1,31 @@
-use pixels::{Pixels, PixelsBuilder, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
 use remotia_buffer_utils::BytesMut;
 use remotia_core::traits::{BorrowMutFrameProperties, FrameProcessor};
 
 use async_trait::async_trait;
-use winit::{
-    event_loop::EventLoop,
-    window::{Window, WindowBuilder},
-};
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
-pub struct WinitRenderer<K> {
+pub struct WinitRenderer<'a, K> {
     buffer_key: K,
-    pixels: Pixels,
-    _window: Window,
+    pixels: Pixels<'a>,
 }
-unsafe impl<K> Send for WinitRenderer<K> {}
+unsafe impl<K> Send for WinitRenderer<'_, K> {}
 
-impl<K> WinitRenderer<K> {
+impl<'a, K> WinitRenderer<'a, K> {
     pub fn new(buffer_key: K, canvas_width: u32, canvas_height: u32) -> Self {
-        let window = WindowBuilder::new().build(&EventLoop::new()).unwrap();
+        let window = WindowBuilder::new()
+            .build(&EventLoop::new().unwrap())
+            .unwrap();
 
-        let pixels = {
-            let surface_texture = SurfaceTexture::new(canvas_width, canvas_height, &window);
-            PixelsBuilder::new(canvas_width, canvas_height, surface_texture)
-                .build()
-                .unwrap()
-        };
+        let surface_texture = SurfaceTexture::new(canvas_width, canvas_height, window);
+        let pixels = Pixels::new(canvas_width, canvas_height, surface_texture).unwrap();
 
-        Self {
-            buffer_key,
-            pixels,
-            _window: window,
-        }
+        Self { buffer_key, pixels }
     }
 }
 
 #[async_trait]
-impl<F, K> FrameProcessor<F> for WinitRenderer<K>
+impl<'a, F, K> FrameProcessor<F> for WinitRenderer<'a, K>
 where
     F: BorrowMutFrameProperties<K, BytesMut> + Send + 'static,
 {

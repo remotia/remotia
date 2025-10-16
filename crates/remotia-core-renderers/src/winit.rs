ca5ba065054be90079aa9e36aa2e88e6e0d08296
aegroto <aegroto@protonmail.com>
2025-10-16 16:31:04 +0200

    feat: separate winit runner to handle gui thread

--- crates/remotia-core-renderers/src/winit.rs
+++ crates/remotia-core-renderers/src/winit.rs
@@ -1,40 +1,78 @@
+use std::{
+    future::Future,
+    sync::{Arc, Mutex},
+};
+
 use pixels::{Pixels, SurfaceTexture};
 use remotia_buffer_utils::BytesMut;
 use remotia_core::traits::{BorrowMutFrameProperties, FrameProcessor};
 
 use async_trait::async_trait;
-use winit::{event_loop::EventLoop, window::WindowBuilder};
+use winit::{
+    dpi::LogicalSize,
+    event_loop::{self, EventLoop},
+    window::WindowBuilder,
+};
 
 pub struct WinitRenderer<'a, K> {
     buffer_key: K,
-    pixels: Pixels<'a>,
+    pixels: Option<Arc<Mutex<Pixels<'a>>>>,
 }
-unsafe impl<K> Send for WinitRenderer<'_, K> {}
 
 impl<'a, K> WinitRenderer<'a, K> {
-    pub fn new(buffer_key: K, canvas_width: u32, canvas_height: u32) -> Self {
-        let window = WindowBuilder::new()
-            .build(&EventLoop::new().unwrap())
+    pub fn new(buffer_key: K) -> Self {
+        Self {
+            buffer_key,
+            pixels: None,
+        }
+    }
+
+    pub fn allocate(&mut self, width: u32, height: u32) -> WinitRunner<'a> {
+        let event_loop = EventLoop::new().unwrap();
+
+        let window = { WindowBuilder::new().build(&event_loop).unwrap() };
+
+        let pixels = Arc::new(Mutex::new({
+            let surface_texture = SurfaceTexture::new(width, height, window);
+            Pixels::new(width, height, surface_texture).unwrap()
+        }));
+
+        self.pixels = Some(pixels.clone());
+
+        WinitRunner { event_loop, pixels }
+    }
+}
+
+pub struct WinitRunner<'a> {
+    event_loop: EventLoop<()>,
+    pixels: Arc<Mutex<Pixels<'a>>>,
+}
+
+unsafe impl<'a> Send for WinitRunner<'a> {}
+
+impl<'a> WinitRunner<'a> {
+    pub fn start(self) {
+        self.event_loop
+            .run(|_event, _elwt| {
+                self.pixels.lock().unwrap().render().unwrap();
+                log::debug!("Rendering buffer...")
+            })
             .unwrap();
-
-        let surface_texture = SurfaceTexture::new(canvas_width, canvas_height, window);
-        let mut pixels = Pixels::new(canvas_width, canvas_height, surface_texture).unwrap();
-
-        Self { buffer_key, pixels }
     }
 }
 
 #[async_trait]
 impl<'a, F, K> FrameProcessor<F> for WinitRenderer<'a, K>
 where
+    K: Send,
     F: BorrowMutFrameProperties<K, BytesMut> + Send + 'static,
 {
     async fn process(&mut self, mut frame_data: F) -> Option<F> {
-        log::debug!("Updating renderer...");
+        log::debug!("Filling pixels buffer...");
 
         let raw_frame_buffer = frame_data.get_mut_ref(&self.buffer_key).unwrap();
-        self.pixels.frame_mut().copy_from_slice(raw_frame_buffer);
-        self.pixels.render().unwrap();
+        let mut pixels = self.pixels.as_mut().unwrap().lock().unwrap();
+        pixels.frame_mut().copy_from_slice(raw_frame_buffer);
 
         Some(frame_data)
     }
